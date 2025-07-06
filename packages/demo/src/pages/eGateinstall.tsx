789340c346dbf4c090010e9d488a5efa2a06059d
JMTDI <carsforall1@gmail.com>
2025-07-05 20:19:39 -0400

    Update eGateinstall.tsx

--- packages/demo/src/pages/eGateinstall.tsx
+++ packages/demo/src/pages/eGateinstall.tsx
@@ -89,7 +89,7 @@ class InstallPageState {
     };
 
     install = async (variant: Variant) => {
-        const apkUrl = `https://egate.carsforall1.workers.dev/?variant=${encodeURIComponent(variant)}`;
+        const apkUrl = `https://jmtdi-github-io.vercel.app/api/proxy?vaiant=${encodeURIComponent(variant)}`;
         let blob: Blob;
         try {
             runInAction(() => {
