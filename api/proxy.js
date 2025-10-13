88c03670396df0542e9ec498e5b3e5438786a8af
JMTDI <carsforall1@gmail.com>
2025-10-13 19:25:51 -0400

    Remove default port fallback in proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -2,7 +2,7 @@ import express from "express";
 import fetch from "node-fetch";
 
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT;
 
 console.log("ENV PORT:", process.env.PORT);
 
