815bde86b10e2c54ea1aa0741983687d9a91a67d
JMTDI <carsforall1@gmail.com>
2026-02-20 22:29:06 +0000

    Update gh-pages to output generated at 036815d

---
+++ _next/static/chunks/pages/_offline-5cee9c5bfb06bb43.js
@@ -0,0 +1,2 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[95],{2644:function(n,u,e){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_offline",function(){return e(4019)}])},4019:function(n,u,e){"use strict";e.r(u),e.d(u,{default:function(){return r}});var t=e(6020),f=e.n(t),c=e(8343);function r(){return(0,c.useEffect)(()=>{f().replace(location.href)},[]),null}}},function(n){n.O(0,[774,888,179],function(){return n(n.s=2644)}),_N_E=n.O()}]);
+//# sourceMappingURL=_offline-5cee9c5bfb06bb43.js.map
--- _next/static/chunks/pages/_offline-91e98413f7ba9726.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[95],{7897:function(n,u,e){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_offline",function(){return e(934)}])},934:function(n,u,e){"use strict";e.r(u),e.d(u,{default:function(){return r}});var t=e(5598),f=e.n(t),c=e(2871);function r(){return(0,c.useEffect)(()=>{f().replace(location.href)},[]),null}}},function(n){n.O(0,[774,888,179],function(){return n(n.s=7897)}),_N_E=n.O()}]);
-//# sourceMappingURL=_offline-91e98413f7ba9726.js.map
